fdd67ee46620fc6d11e18a2be2f177b7a522beb9
Yury <yurykoropenko@gmail.com>
2017-03-10 17:31:52 +0200

    commit 2

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
 		$(this).removeClass('h-nav__item-active');
 		$(this).children('.h-nav__sub').stop(true, false).slideUp();
 	});
+
+/**/
+	$(function() {
+		$(window).scroll(function() {
+			if($(this).scrollTop() != 0)
+				$('.button-up__wp').fadeIn(300);
+			else
+			$('.button-up__wp').fadeOut(300);
+		});
+	});
+	$('.button-up').on('click', function() {
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+
+/**/
+	$('.h-nav__mob-button').on('click', function() {
+		$('.header__h-nav').slideToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.header__h-nav').length) return;
+				$('.header__h-nav').fadeOut();
+				event.stopPropagation();
+			});
+		return false;
+	});
 });
