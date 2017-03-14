278360f14958d9e1792a807207158a2c3b4faa9a
Yury <yurykoropenko@gmail.com>
2017-03-14 17:14:56 +0200

    commit 4

--- app/js/main.js
+++ app/js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		$('.header__h-nav').slideToggle();
 		$(document).click(function(event) {
 			if ($(event.target).closest('.header__h-nav').length) return;
-				$('.header__h-nav').fadeOut();
+				$('.header__h-nav').slideUp();
 				event.stopPropagation();
 			});
 		return false;
@@ -39,12 +39,26 @@ $(document).ready(function() {
 	});
 
 /**/
+	$('.mob-sidebar__button').on('click', function() {
+		$('.sidebar').slideToggle();
+		$(document).click(function(event) {
+			if ($(event.target).closest('.sidebar').length) return;
+				$('.sidebar').slideUp();
+				event.stopPropagation();
+			});
+		return false;
+	});
+/**/
 	$('.card__c-color-item span').on('click', function() {
 		var color = $(this).text();
 		$('.card__c-color-item span').removeClass('active');
 		$(this).addClass('active');
 		$('.card__c-color-name').text(color);
 	});
-	var ht = $('.ht').height();
-	$('.sidebar').height(ht);
+
+/**/
+	if (document.body.clientWidth>991) {
+		var ht = $('.ht').height();
+		$('.sidebar').height(ht);
+	}
 });
